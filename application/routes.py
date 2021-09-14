4c6e16d5dce38f624d167781891c6b7c5302cf79
Sean <snerie6@gmail.com>
2021-09-14 22:15:10 +0800

    UI update

--- application/routes.py
+++ application/routes.py
@@ -9,8 +9,14 @@ import plotly.graph_objects as go
 
 @app.route("/")
 def index():
-	country = "North America"
-	year = 'Y2000'
+	# blue colorscale for the maps
+	fig1_colors = ["#4e79a7","#f28e2b","#e15759",
+					"#76b7b2","#59a14f"]
+	# fig1_colors = ["Coal":"#4e79a7","Oil":"#f28e2b","Solar":"#e15759",
+	# 				"Hydro":"#76b7b2","Wind":"#59a14f"]
+	fig3_colors = ["#6baed6", "#57a0ce", "#4292c6", "#3082be", "#2171b5", "#1361a9", "#08519c", "#0b4083", "#08306b"]
+	country = "Costa Rica"
+	year = 'Y2014'
 
 	#graph one
 	consumption_electricity_df = pd.DataFrame(pd.read_csv("data/Electricity_Consumption.csv"))
@@ -24,11 +30,12 @@ def index():
 	temp_df['kWh'] = temp_df['kWh'].astype(float)
 
 	fig1 = px.line(temp_df, x="Years", y="kWh",  title="Electricity Consumption",
-		width=300, height=270)
+		width=350, height=270)
 	fig1.update_layout(
 		paper_bgcolor="black",
+		plot_bgcolor='black',
 		font_color="white")
-	fig1.update_traces(line_color='#4e79a7')
+	fig1.update_traces(line_color='#f28e2b')
 	graph1JSON = json.dumps(fig1, cls=plotly.utils.PlotlyJSONEncoder)
 
 	#Graph Two
@@ -38,21 +45,21 @@ def index():
 	test_data2 = test_data2.drop(['Country', 'index'], axis=1)
 
 	fig2 = px.area(test_data2, x="Year", y=['Coal', 'Hydro', 'Solar', 'Oil', 'Wind'],
-		title="Electricity Production", width=300, height=270)	
+		title="Electricity Production", width=350, height=270)	
 	fig2.update_layout(
 		paper_bgcolor="black",
+		plot_bgcolor='black',
 		font_color="white")
 	graph2JSON = json.dumps(fig2, cls = plotly.utils.PlotlyJSONEncoder)
 
 	# Graph three\
 	access_electricity_df = pd.DataFrame(pd.read_csv("data/Access_To_Electricity.csv"))
-	year = 'Y2000'
 	test_data3 = access_electricity_df.loc[:,['Country ','Code',year]]
 
 	choropleth_fig = go.Figure(data=go.Choropleth(locations=test_data3['Code'], 
 		z=test_data3[year],
 		text= test_data3['Country '],
-		colorscale='reds',
+		colorscale=fig3_colors,
 		autocolorscale=False,
 		reversescale=False,
 		marker_line_color='#313131',
@@ -60,8 +67,9 @@ def index():
 		colorbar_title='Access (%)'))
 	choropleth_fig.update_layout(title_text = 'Population % with access to electricity',
 		geo=dict(showframe=False, showcoastlines=False, projection_type='equirectangular'),
-		width=800, height=800,
+		width=800, height=520,
 		paper_bgcolor="black",
+		plot_bgcolor='black',
 		font_color="white")
 
 	# df = px.data.medals_wide()
